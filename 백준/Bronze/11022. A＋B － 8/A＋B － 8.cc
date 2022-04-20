e250cf00223d223d3091b12543ab9cca93da66a4
ohmingi <87136887+ohmingi@users.noreply.github.com>
2022-04-20 17:48:34 +0900

    [Bronze III] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a;
+	
+	int i{1};
+
+	while(a>0){
+		cin >> b >> c;
+		cout << "Case #"<< i<< ": "<< b<<" + "<<c<<" = " << b + c << "\n";
+		a--;
+		i++;
+	}
+}
