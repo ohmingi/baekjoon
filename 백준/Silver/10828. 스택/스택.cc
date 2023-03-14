f5fd0d15d400aa3ddf036065a24a2212239a4b08
ohmingi <87136887+ohmingi@users.noreply.github.com>
2023-03-14 21:47:58 +0900

    [Silver IV] Title: 스택, Time: 332 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,130 +1,61 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-class Node {
+class stack {
 private:
-	int value;
-	Node* next;
+    int datasize = 0;
+    
+    int arr[100000] = {};
 public:
-	Node(int value, Node* node = nullptr) {
-		this->next = node;
-		this->value = value;
-	};
-	int getData();
-	Node* getNext();
-	void setData(int);
-	void setNext(Node*);
-};
+    
+    bool empty() {
+        return datasize == 0;
+    }
+    int top() {
+        if (empty()) return -1;
+        return arr[datasize - 1];
+    }
+    void push(int x) {
+        arr[datasize++] = x;
+    }
+    void pop() {
+        if (datasize == 0) {
+            cout << -1 << '\n';
+            return;
+        }
+        cout << top() << '\n';
+        datasize--;
+    }
 
-class Stack {
-public:
-	bool empty();
-	int size();
-	int top();
-	void push(int);
-	int pop();
-	/*void display();*/
-private:
-	Node* TopNode;
-	int datasize=0;
+    void size() {
+        cout << datasize << "\n";
+    }
 };
 
 int main() {
+    int t, n;
+    cin >> n;
+    string init;
+    stack s;
 
-	Stack arr;
-	int n;
-	string str;
-	cin >> n;
-
-	while (n--) {
-		cin >> str;
-		if (str == "push") {
-			int value;
-			cin >> value;
-			arr.push(value);
-		}
-		else if (str=="pop") {
-			cout <<arr.pop()<<"\n";
-		}
-		
-		else if (str=="size") {
-			cout << arr.size()<<"\n";
-		}
-
-		else if (str=="empty") {
-			cout << arr.empty()<<"\n";
-		}
-		else if (str=="top") {
-			cout << arr.top()<<"\n";
-		}
-	}
-
-
+    while (n--) {
+        cin >> init;
+        if (init == "empty") {
+            cout << s.empty() << '\n';
+        }
+        else if (init == "top") {
+            cout << s.top() << '\n';
+        }
+        else if (init == "pop") {
+            s.pop();
+        }
+        else if (init=="size") {
+            s.size();
+        }
+        else {
+            int x;
+            cin >> x;
+            s.push(x);
+        }
+    }
 }
-
-
-int Node::getData() {
-	return value;
-}
-
-Node* Node::getNext() {
-	return next;
-}
-
-void Node::setData(int data) {
-	this->value = data;
-}
-
-void Node::setNext(Node* node) {
-	this->next = node;
-}
-
-
-
-
-bool Stack::empty() {
-	if (datasize == 0) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-	
-}
-
-int Stack::size() {
-	return datasize;
-}
-
-int Stack::top() {
-	if (empty()) {
-		return -1;
-	}
-	return TopNode->getData();
-}
-
-void Stack::push(int data) {
-	Node* node = new Node(data, TopNode);
-	TopNode = node;
-	datasize++;
-}
-
-int Stack::pop() {
-	if (datasize == 0) {
-		return -1;
-	}
-	else {
-		int topvlaue = top();
-		Node* node = TopNode;
-		TopNode = node->getNext();
-		delete node;
-		datasize--;
-		return topvlaue;
-	}
-	
-}
-
-
-
