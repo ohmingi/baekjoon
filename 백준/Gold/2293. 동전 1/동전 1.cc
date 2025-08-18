86761eea3e923a0b8654753b1972d555d5e272b3
ohmingi <87136887+ohmingi@users.noreply.github.com>
2025-08-18 19:33:48 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+vector<int> dp;
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+
+    v.assign(n+1,0);
+    dp.assign(k+1,0);
+    
+
+    for(int i = 1; i <= n; i++){
+        cin >> v[i];
+    }
+
+    dp[0] = 1;
+
+    for(int i = 1; i <= n; i++){
+        for(int t = v[i]; t <= k; t++){
+            dp[t] += dp[t - v[i]];
+        }
+    }
+    
+    cout << dp[k];
+}
