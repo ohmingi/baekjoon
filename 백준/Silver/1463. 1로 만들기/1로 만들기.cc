447d5fa612d71f1c346d716027e8640ba18b2b01
ohmingi <87136887+ohmingi@users.noreply.github.com>
2025-08-19 18:40:41 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int dp[1000001];
+
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+    dp[5] = 3;
+
+    for(int i = 6; i<=n; i++){
+       
+        
+        if(i%3 != 0 && i%2 !=0){
+            dp[i] = dp[i-1]+1;
+        }
+        else if(i%2 == 0 && i%3 == 0){
+            dp[i] = min({dp[i-1]+1, dp[i/2]+1, dp[i/3]+1});
+        }
+        else if(i%2 == 0){
+            dp[i] = min(dp[i-1]+1, dp[i/2]+1); 
+        }
+        else{
+            dp[i] = min(dp[i-1]+1, dp[i/3]+1); 
+        }
+    }
+
+    cout << dp[n];
+}
