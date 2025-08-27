5dc7d2af68a73bb7532a1443e9227f736cbe0025
ohmingi <87136887+ohmingi@users.noreply.github.com>
2025-08-27 21:03:26 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -5,43 +5,48 @@
 using namespace std;
 
 int n;
-vector<vector<int>> v;
-bool visited[101];
 int graph[101][101];
+int result[101][101];
+bool visited[101];
 
-void dfs(int node, int aw, int f){
-    visited[node] = 1;
-
-    for(int i = 1; i <= n; i++){
-        if(v[node][i] == 1 && i == aw) graph[i][i] = 1;
-        if(v[node][i] == 1 && !visited[i]){
-            if(i == f) {
-                graph[aw][f] = 1;
-                return;
-            }
-            dfs(i, aw, f);
+void dfs_matrix(int current_node) {
+    for (int next_node = 1; next_node <= n; ++next_node) {
+        if (graph[current_node][next_node] == 1 && !visited[next_node]) {
+            visited[next_node] = true;
+            dfs_matrix(next_node);
         }
     }
 }
 
-int main(){
-    cin >> n;
-    v.assign(n+1, vector<int>(n+1));
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    for(int i = 1; i <= n; i++){
-        for(int t = 1; t <= n; t++){
-            cin >> v[i][t];
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> graph[i][j];
         }
     }
 
-    for(int i = 1; i <= n; i++){
-        for(int t = 1; t <= n; t++){
-            dfs(i,i,t);
-            memset(visited, 0, sizeof(visited));
-            cout << graph[i][t] << " ";
-        }
+    for (int i = 1; i <= n; i++) {
+        memset(visited, false, sizeof(visited));
+        
+        dfs_matrix(i);
 
+        for (int j = 1; j <= n; j++) {
+            if (visited[j]) {
+                result[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << result[i][j] << " ";
+        }
         cout << "\n";
     }
 
+    return 0;
 }
